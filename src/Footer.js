3ca816796049dc66c96ca7c511da77b9fd9c47ad
Shannon2023Fall <148981251+Shannon2023Fall@users.noreply.github.com>
2023-11-22 19:07:35 +0100

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -1,1 +1,17 @@
+import React from 'react';
+class Footer extends React.Component{
+    render(){
+        return <h1> Hello, {this.props.name}!</h1>;
+    }
+}
 
+function App(){
+    return(
+    <div>
+    <small>
+    Copyright © 2023 ShanshansWork. All Rights Reserved.
+    </small>
+    </div>
+    );
+}
+export default App;
